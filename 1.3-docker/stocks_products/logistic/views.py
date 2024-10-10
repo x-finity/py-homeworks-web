79877ac365b8062ad4712f54108630e04c31d702
x-finity <x.finity777@gmail.com>
2024-10-11 02:30:36 +0300

    first commit

---
+++ 1.3-docker/stocks_products/logistic/views.py
@@ -0,0 +1,26 @@
+from rest_framework.filters import SearchFilter
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.viewsets import ModelViewSet
+from django_filters.rest_framework import DjangoFilterBackend
+
+from logistic.models import Product, Stock
+from logistic.serializers import ProductSerializer, StockSerializer
+
+
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    # при необходимости добавьте параметры фильтрации
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    search_fields = ['description']
+    filterset_fields = ['title']
+    pagination_class = PageNumberPagination
+
+
+class StockViewSet(ModelViewSet):
+    queryset = Stock.objects.all()
+    serializer_class = StockSerializer
+    # при необходимости добавьте параметры фильтрации
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filterset_fields = ['products']
+    pagination_class = PageNumberPagination
